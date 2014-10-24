d816c00e96fd58060bcd663bacb19da2391d754e
Noah Holmes <noah.r.holmes@gmail.com>
2014-10-24 16:33:19 -0400

    added tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,16 +2,36 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
+  def valid_user_attributes
+    {name: Faker::Name.name,
+     email: Faker::Internet.email,
+     password: "password",
+     password_confirmation: "password"}
+  end
+
+  def invalid_user_attributes
+    {name: "",
+     email: "",
+     password: "",
+     password_confirmation: ""}
+  end
+
   test "should get new" do
     get :new
     assert_response :ok
   end
 
-  # test "should render new with invalid information" do
-  #     setup { post :create, { name: "",
-  #     email: "",
-  #     password: "",
-  #     password_confirmation: ""  }
-  #     assert_template :new
-  # end
+  context "POST :create" do
+    context "when I send invalid information" do
+      setup { post :create, { user: invalid_user_attributes } }
+
+      should "re-render the form" do
+        assert_template :new
+      end
+    end
+    should "instantiate an invalid user object" do
+        assert assigns["user"], "Should have a user"
+        assert assigns["user"].invalid?, "Should have an invalid user"
+      end
+    end
 end
