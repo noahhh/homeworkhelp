acbb422f523c08b380175b023191ff14072f8ae4
Torey Stapleton <torey.stapleton@yahoo.com>
2014-10-25 12:14:30 -0400

    minor fixes, added pagination

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,7 +1,9 @@
 class Problem < ActiveRecord::Base
+  paginates_per 10
+
   belongs_to :user
   has_many :notes, dependent: :destroy
-  
+
   validates :user, presence: true
   validates :body, presence: true
   validates :title, presence: true
