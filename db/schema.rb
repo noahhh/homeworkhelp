f3684443c17bd5891936b40b3b53a0c187e56b5d
Noah Holmes <noah.r.holmes@gmail.com>
2014-10-25 22:04:39 -0400

    adding resolved to problems

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025173438) do
+ActiveRecord::Schema.define(version: 20141026010830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20141025173438) do
     t.string   "title"
     t.string   "body"
     t.datetime "published_date"
-    t.boolean  "solved",         default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "resolved", default: false
